466d4d2b9c7603710a0a7f2f16eaaf8cffda8db2
Evgenii <voznukzhenia@gmail.com>
2021-09-06 16:32:15 +0300

    update

--- src/components/LearningWays.vue
+++ src/components/LearningWays.vue
@@ -2,6 +2,79 @@
   <div class="lw">
     <div class="lw__container container">
       <SectionTitle :title="'Направления обучения'" />
+      <div class="lw__grid">
+        <div class="way">
+          <h1 class="way__title">Дизайн интерьера жилых помещений</h1>
+          <p class="way__description">
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Possimus
+            aliquid eligendi minima ab voluptatem? Consectetur, dicta adipisci.
+            Ullam praesentium doloribus dolorem error velit voluptatum provident
+            suscipit, maxime, perferendis distinctio cupiditate?
+          </p>
+          <p class="way__date">Начало 6 июля</p>
+          <a class="way__link">Подробнее</a>
+        </div>
+
+        <div class="way">
+          <h1 class="way__title">Индивидуальное занятие</h1>
+          <p class="way__description">
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Possimus
+            aliquid eligendi minima ab voluptatem? Consectetur, dicta adipisci.
+            Ullam praesentium doloribus dolorem error velit voluptatum provident
+            suscipit, maxime, perferendis distinctio cupiditate?
+          </p>
+          <p class="way__date">Начало 6 июля</p>
+          <a class="way__link">Подробнее</a>
+        </div>
+
+        <div class="way">
+          <h1 class="way__title">Программа ArchiCAD</h1>
+          <p class="way__description">
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Possimus
+            aliquid eligendi minima ab voluptatem? Consectetur, dicta adipisci.
+            Ullam praesentium doloribus dolorem error velit voluptatum provident
+            suscipit, maxime, perferendis distinctio cupiditate?
+          </p>
+          <p class="way__date">Начало 6 июля</p>
+          <a class="way__link">Подробнее</a>
+        </div>
+
+         <div class="way">
+          <h1 class="way__title">Скетч для ландшафтных дизайнеров</h1>
+          <p class="way__description">
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Possimus
+            aliquid eligendi minima ab voluptatem? Consectetur, dicta adipisci.
+            Ullam praesentium doloribus dolorem error velit voluptatum provident
+            suscipit, maxime, perferendis distinctio cupiditate?
+          </p>
+          <p class="way__date">Начало 6 июля</p>
+          <a class="way__link">Подробнее</a>
+        </div>
+
+         <div class="way">
+          <h1 class="way__title">Рисование с нуля</h1>
+          <p class="way__description">
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Possimus
+            aliquid eligendi minima ab voluptatem? Consectetur, dicta adipisci.
+            Ullam praesentium doloribus dolorem error velit voluptatum provident
+            suscipit, maxime, perferendis distinctio cupiditate?
+          </p>
+          <p class="way__date">Начало 6 июля</p>
+          <a class="way__link">Подробнее</a>
+        </div>
+
+         <div class="way">
+          <h1 class="way__title">Скетч в интерьере</h1>
+          <p class="way__description">
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Possimus
+            aliquid eligendi minima ab voluptatem? Consectetur, dicta adipisci.
+            Ullam praesentium doloribus dolorem error velit voluptatum provident
+            suscipit, maxime, perferendis distinctio cupiditate?
+          </p>
+          <p class="way__date">Начало 6 июля</p>
+          <a class="way__link">Подробнее</a>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -16,6 +89,37 @@ export default {
 </script>
 
 <style lang="scss">
+.way {
+  text-align: center;
+  display: flex;
+  flex-direction: column;
+  gap: 30px;
+  width: 400px;
+  height: 400px;
+  border-radius: 50%;
+  padding: 20px;
+
+  background-color: #f3f2f2;
+
+  font-family: "Montserrat";
+
+  &__title {
+    padding-top: 90px;
+    font-size: 22px;
+    color: #D04D30;
+  }
+
+  &__description {
+    color: rgba(24, 24, 24, 0.87);
+    line-height: 22px;
+  }
+
+  &__link {
+    font-weight: bold;
+    color: #D04D30;
+    cursor: pointer;
+  }
+}
 .lw {
   display: flex;
   justify-content: center;
@@ -23,11 +127,19 @@ export default {
   background-size: cover;
   background-repeat: no-repeat;
   background-position: center;
-  height: 1200px;
+  padding-bottom: 100px;
+
+  &__grid {
+    display: grid;
+    gap: 30px;
+    grid-template-columns: repeat(3, 1fr);
+  }
 
   &__container {
     display: flex;
-    align-items: flex-start;
+    flex-direction: column;
+    align-items: center;
+    gap: 100px;
     padding-top: 50px;
   }
 }
